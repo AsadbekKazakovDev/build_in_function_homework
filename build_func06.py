8b45f3bd88ace515f9e7871b68879ce08a2205d3
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-06-20 12:10:56 +0500

    6-masala yechimi

--- build_func06.py
+++ build_func06.py
@@ -1,4 +1,5 @@
 def main(a):
+    
     """Float type variable 'a' is given. Round the result to 2 decimal places in README.md file.
     https://github.com/codeschool43/Build_in_function_homework#build_func06
 
