792c1f0dd9bd360cdb639609236e372182612351
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-06-20 11:59:46 +0500

    1-masala yechimi

--- build_func01.py
+++ build_func01.py
@@ -1,10 +1,12 @@
 def main(number):
     """A integer type variable 'number' is given. Return the absolute value of a "number". 
-
+    
     Args:
         number (int): integer
 
     Returns:
         int: the absolute value of a "number"
     """
-    return
+    return number*-1
+number = -8
+print(main(number))
